1ce56afc355343e5ad59ec7ffc0e68cc83c2776d
Artur Bondarenko <ua.bondarenko.artur@gmail.com>
2019-09-09 21:12:34 +0300

    bot minor updates

--- bot.py
+++ bot.py
@@ -1,22 +1,32 @@
+from os import environ, mkdir, path
+
+import dataset
+import jsonpickle
 import telebot
-from os import environ
-import sqlite3
 
-bot = telebot.TeleBot(environ['TELEGRAM_TOKEN'])
+BOT = telebot.TeleBot(environ['TELEGRAM_TOKEN'])
+try:
+    if not path.exists('.data'):
+        mkdir(".data")
+except OSError:
+    print("Cannot create folder .data")
+DB = dataset.connect('sqlite:///.data/telebot.db')
 
-bot_text = '''
-Чё, как оно?
-Я короче быстренько поднял бота на публичной платформе, хз будет ли работать
-Сорсы тут https://glitch.com/~{}
-'''.format(environ['PROJECT_NAME'])
 
-@bot.message_handler(commands=['start', 'help'])
+@BOT.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, bot_text)
-  
-# Обработчик сообщений, содержащих документ с mime_type 'text/plain' (обычный текст)
-@bot.message_handler(func=lambda message: message.document.mime_type == 'text/plain', content_types=['document'])
-def handle_text_doc(message):
-    pass
-  
-bot.set_webhook("https://{}.glitch.me/{}".format(environ['PROJECT_NAME'], environ['TELEGRAM_TOKEN']))
+    result = DB.query('SELECT * FROM welcomes ORDER BY RANDOM() LIMIT 1')
+    for row in result:
+        BOT.reply_to(message, row['str'].format(environ['PROJECT_NAME']))
+
+
+@BOT.message_handler(func=lambda message: True)
+def store_all(message):
+    msg_table = DB['messages']
+    msg_table.insert(dict(chat_id=message.chat.id, message=message.text,
+                          message_id=message.message_id,
+                          json=jsonpickle.encode(message)))
+
+
+BOT.set_webhook("https://{}.glitch.me/{}".format(environ['PROJECT_NAME'],
+                                                 environ['TELEGRAM_TOKEN']))
--- server.py
+++ server.py
@@ -1,29 +1,31 @@
-import flask, telebot
-from bot import bot
+import flask
+import telebot
 
-app = flask.Flask(__name__)
-WEBHOOK_URL_PATH = "/{}".format(bot.token)
-index = open('static/index.html').read()
+from bot import BOT
+
+APP = flask.Flask(__name__)
+WEBHOOK_URL_PATH = "/{}".format(BOT.token)
+INDEX = open('static/index.html').read()
 
 
 # Process index page
-@app.route('/')
+@APP.route('/')
 def root():
     print('index!')
-    return index # 'xd' # flask.send_from_directory('/static', 'index.html')
+    return INDEX  # 'xd' # flask.send_from_directory('/static', 'index.html')
 
 
 # Process webhook calls
-@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+@APP.route(WEBHOOK_URL_PATH, methods=['POST'])
 def webhook():
     if flask.request.headers.get('content-type') == 'application/json':
         json_string = flask.request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
+        BOT.process_new_updates([update])
         return ''
     else:
         flask.abort(403)
 
+
 if __name__ == "__main__":
-  
-  app.run()
+    APP.run()
