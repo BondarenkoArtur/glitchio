454baff435076ae603e8c02630a461533a72fa68
Artur Bondarenko <ua.bondarenko.artur@gmail.com>
2019-09-09 21:25:29 +0300

    db now local

--- bot.py
+++ bot.py
@@ -10,19 +10,20 @@ try:
         mkdir(".data")
 except OSError:
     print("Cannot create folder .data")
-DB = dataset.connect('sqlite:///.data/telebot.db')
 
 
 @BOT.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    result = DB.query('SELECT * FROM welcomes ORDER BY RANDOM() LIMIT 1')
+    db = dataset.connect('sqlite:///.data/telebot.db')
+    result = db.query('SELECT * FROM welcomes ORDER BY RANDOM() LIMIT 1')
     for row in result:
         BOT.reply_to(message, row['str'].format(environ['PROJECT_NAME']))
 
 
 @BOT.message_handler(func=lambda message: True)
 def store_all(message):
-    msg_table = DB['messages']
+    db = dataset.connect('sqlite:///.data/telebot.db')
+    msg_table = db['messages']
     msg_table.insert(dict(chat_id=message.chat.id, message=message.text,
                           message_id=message.message_id,
                           json=jsonpickle.encode(message)))
